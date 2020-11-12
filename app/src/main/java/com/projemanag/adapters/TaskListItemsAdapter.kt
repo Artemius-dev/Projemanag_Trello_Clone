93eea40e5ab426624b710845d5bb00b7d636b7d1
AnyMac Store <anymacstore@AnyMacs-MBP.Dlink>
2020-11-12 21:04:42 +0200

    Added functionality to add Tasks and save them into DB

--- app/src/main/java/com/projemanag/adapters/TaskListItemsAdapter.kt
+++ app/src/main/java/com/projemanag/adapters/TaskListItemsAdapter.kt
@@ -1,13 +1,16 @@
 package com.projemanag.adapters
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.projemanag.R
+import com.projemanag.activities.TaskListActivity
 import com.projemanag.models.Task
 import kotlinx.android.synthetic.main.item_task.view.*
 
@@ -41,7 +44,90 @@ open class TaskListItemsAdapter(
                 holder.itemView.tv_add_task_list.visibility = View.GONE
                 holder.itemView.ll_task_item.visibility = View.VISIBLE
             }
+
+            holder.itemView.tv_task_list_title.text = model.title
+            holder.itemView.tv_add_task_list.setOnClickListener {
+                holder.itemView.tv_add_task_list.visibility = View.GONE
+                holder.itemView.cv_add_task_list_name.visibility = View.VISIBLE
+            }
+
+            holder.itemView.ib_close_list_name.setOnClickListener {
+                holder.itemView.tv_add_task_list.visibility = View.VISIBLE
+                holder.itemView.cv_add_task_list_name.visibility = View.GONE
+            }
+
+            holder.itemView.ib_done_list_name.setOnClickListener {
+                val listName = holder.itemView.et_task_list_name.text.toString()
+
+                if(listName.isNotEmpty()) {
+                    if(context is TaskListActivity) {
+                        context.createTaskList(listName)
+                    }
+                } else {
+                    Toast.makeText(context, "Please Enter List Name.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
         }
+
+        holder.itemView.ib_edit_list_name.setOnClickListener {
+            holder.itemView.et_edit_task_list_name.setText(model.title)
+            holder.itemView.ll_title_view.visibility = View.GONE
+            holder.itemView.cv_edit_task_list_name.visibility = View.VISIBLE
+        }
+
+        holder.itemView.ib_close_editable_view.setOnClickListener {
+            holder.itemView.ll_title_view.visibility = View.VISIBLE
+            holder.itemView.cv_edit_task_list_name.visibility = View.GONE
+        }
+
+        holder.itemView.ib_done_edit_list_name.setOnClickListener {
+            val listName = holder.itemView.et_edit_task_list_name.text.toString()
+
+            if(listName.isNotEmpty()) {
+                if(context is TaskListActivity) {
+                    context.updateTaskList(position, listName, model)
+                }
+            } else {
+                Toast.makeText(context, "Please Enter a List Name.",
+                    Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+        holder.itemView.ib_delete_list.setOnClickListener {
+            alertDialogForDeleteList(position, model.title)
+        }
+
+    }
+
+    private fun alertDialogForDeleteList(position: Int, title: String) {
+        val builder = AlertDialog.Builder(context)
+        // set title for alert dialog
+        builder.setTitle("Alert")
+        // set message for alert dialog
+        builder.setMessage("Are you sure you want to delete $title.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        // performing positive action
+        builder.setPositiveButton("Yes") { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+            if (context is TaskListActivity) {
+                context.deleteTaskList(position)
+            }
+        }
+
+        // performing negative action
+        builder.setNegativeButton("No") { dialogInterface, which ->
+            dialogInterface.dismiss()
+        }
+
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false) // Will not allow user to cancel after
+        alertDialog.show() // show the dialog to UI
     }
 
     private fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
