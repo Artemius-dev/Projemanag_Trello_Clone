fd9a6c83accc4c0ef6b8c220c14f0d6403975159
AnyMac Store <anymacstore@AnyMacs-MBP.Dlink>
2020-11-13 22:53:38 +0200

    Added Card Details Activity and basic functionality for it

--- app/src/main/java/com/projemanag/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/projemanag/activities/CardDetailsActivity.kt
@@ -1,12 +1,63 @@
 package com.projemanag.activities
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
 import com.projemanag.R
+import com.projemanag.models.Board
+import com.projemanag.utils.Constants
+import kotlinx.android.synthetic.main.activity_card_details.*
+import kotlinx.android.synthetic.main.activity_members.*
 
-class CardDetailsActivity : AppCompatActivity() {
+class CardDetailsActivity : BaseActivity() {
+
+    private lateinit var mBoardDetails: Board
+    private var mTaskListPosition = -1
+    private var mCardPosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_details)
+
+        getIntentData()
+        setupActionBar()
+
+        et_name_card_details.setText(
+            mBoardDetails
+                .taskList[mTaskListPosition]
+                .cards[mCardPosition].name
+        )
+        et_name_card_details.setSelection(et_name_card_details.text.toString().length)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar_card_details_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = mBoardDetails
+                .taskList[mTaskListPosition]
+                .cards[mCardPosition].name
+        }
+
+        toolbar_card_details_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_delete_card, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun getIntentData() {
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)
+        }
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITiON)) {
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITiON, -1)
+        }
     }
 }
--- app/src/main/java/com/projemanag/activities/TaskListActivity.kt
+++ app/src/main/java/com/projemanag/activities/TaskListActivity.kt
@@ -36,7 +36,7 @@ class TaskListActivity : BaseActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && resultCode == MEMBERS_REQUEST_CODE) {
+        if (resultCode == Activity.RESULT_OK && requestCode == MEMBERS_REQUEST_CODE || requestCode == CARD_DETAILS_REQUEST_CODE) {
             showProgressDialog(resources.getString(R.string.please_wait))
             FirestoreClass().getBoardDetails(this, mBoardDocumentId)
         } else {
@@ -50,7 +50,11 @@ class TaskListActivity : BaseActivity() {
     }
 
     fun cardDetails(taskListPosition: Int, cardPosition: Int) {
-        startActivity(Intent(this, CardDetailsActivity::class.java))
+        val intent = Intent(this, CardDetailsActivity::class.java)
+        intent.putExtra(Constants.BOARD_DETAIL, mBoardDetails)
+        intent.putExtra(Constants.TASK_LIST_ITEM_POSITION, taskListPosition)
+        intent.putExtra(Constants.CARD_LIST_ITEM_POSITiON, cardPosition)
+        startActivityForResult(intent, CARD_DETAILS_REQUEST_CODE)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -164,5 +168,6 @@ class TaskListActivity : BaseActivity() {
 
     companion object {
         const val MEMBERS_REQUEST_CODE: Int = 13
+        const val CARD_DETAILS_REQUEST_CODE: Int = 14
     }
 }
