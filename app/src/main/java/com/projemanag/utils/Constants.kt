23e80e53eed659077641e96e355c687c5037248e
AnyMac Store <anymacstore@AnyMacs-MBP.Dlink>
2020-11-13 20:20:15 +0200

    Added functionality to add members to a Board

--- app/src/main/java/com/projemanag/utils/Constants.kt
+++ app/src/main/java/com/projemanag/utils/Constants.kt
@@ -21,6 +21,8 @@ object Constants {
     const val DOCUMENT_ID: String = "documentId"
     const val TASK_LIST: String = "taskList"
     const val BOARD_DETAIL: String = "board_detail"
+    const val ID: String = "id"
+    const val EMAIL: String = "email"
 
     fun showImageChooser(activity: Activity) {
         var galleryIntent = Intent(
